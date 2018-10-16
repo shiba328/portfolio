b3ccea799e6c89f3c5f243ba37b09725ad6c8da0
shiba <project.funnal@gmail.com>
2018-10-16 20:19:29 +0900

    コンテンツコンポーネント

---
+++ components/index/program.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+    div(class="mb-5")
+      h1 Programming
+      v-layout(row wrap)
+        v-flex(
+            xs12 sm6
+            md4
+            v-for="p in postData"
+            :key="p.id"
+          )
+            v-card(flat :href="p.link")
+                v-img(
+                    :src="require('../../assets/img/post/'+ p.img)"
+                    aspect-ratio="1.9"
+                )
+                v-card-text
+                    h2 {{p.title}}
+                    div {{p.discript}}
+                    v-chip( v-for="s in p.skills" small label outline) {{s}} 
+</template>
+
+<script>
+export default {
+  computed: {
+      postData: () => [
+        {
+          id:1,
+          link:'https://gs-shiba.sakura.ne.jp/lab02/',
+          title: 'Flying Pizaa!',
+          img: 'flyingPizaa.png',
+          discript: 'HTML+JS+localstorageで簡易ゲーム。結果をlocalstorageに保存します',
+          tag:['HTML/CSS','jQuery','LocalStorage'],
+          period: '16h'
+        },
+        {
+          id:2,
+          link:'https://gs-shiba.sakura.ne.jp/lab03/',
+          title: 'Janken Wars',
+          img: 'JankenWars.png',
+          discript: 'HTML+JS+ウェブカメラでじゃんけん。ウェブカメラで出し手の上に手をかざすとじゃんけんします',
+          tag:['HTML/CSS','jQuery','canvas','webカメラ'],
+          period: '24h'
+        },{
+          id:3,
+          link:'https://gs-shiba.sakura.ne.jp/lab04/',
+          title: 'iOSメモ帳クローン(マークダウン表示)',
+          img: 'memo1.png',
+          discript: 'iOSのメモをそのままJSで表現。マークダウン表示に変更できます',
+          skills:['HTML/CSS','jQuery','LocalStorage','json'],
+          period: '12h'
+        },{
+          id:4,
+          link:'https://gs-shiba.sakura.ne.jp/lab05/',
+          title: '四文字熟語を答えよう！',
+          img: 'yonmoji.png',
+          discript: '４文字熟語ドリル。空白に文字を入れて４文字熟語を答えよう',
+          skills:['HTML/CSS','jQuery','json'],
+          period: '8h'
+        },{
+          id:5,
+          link:'https://gs-shiba.sakura.ne.jp/lab06/',
+          title: 'OldMaid',
+          img: 'oldmaid.png',
+          discript: 'Firebaseを利用して、リアルタイムババ抜き対戦',
+          skills:['HTML/CSS','jQuery','firebase','LocalStorage'],
+          period: '38h'
+        }
+      ],
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/index/skill.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+    div(id="skillArea" class="mb-5")
+      h1 Skill
+      v-layout(row justify-center align-center text-xs-center)
+        v-flex(xs5): h2 BackEnd
+        v-flex(xs2): v-divider
+        v-flex(xs5): h2 FrontEnd
+      v-layout(row justify-center align-center text-xs-center)
+        v-flex(xs5)
+          v-layout(row justify-center align-center text-xs-center)
+            v-flex(xs6)
+              v-icon(color="red lighten-1") favorite
+              div Like
+            v-flex(xs6) 
+              v-icon(color="amber lighten-1")  battery_charging_full
+              div Power
+        v-flex(xs2)
+        v-flex(xs5)
+          v-layout(row justify-center align-center text-xs-center)
+            v-flex(xs6)
+              v-icon(color="amber lighten-1") battery_charging_full
+              div Power
+            v-flex(xs6)
+              v-icon(color="red lighten-1") favorite
+              div() Like
+      template(v-for="(skill,i) in Fskills")
+        v-layout( :key="skill[0]" row justify-center align-center text-xs-center)
+          v-flex(xs5)
+            v-layout(row wrap justify-center align-center text-xs-center)
+              v-flex(xs6 style="position: relative;")
+                div(style="position: absolute;z-index: 2;color:#fff;margin:0 5px") {{ Bskills[i][2] }}%
+                v-progress-linear(
+                    color="red lighten-1"
+                    height="20"
+                    :value="Bskills[i][2]"
+                    style="margin:0"
+                )
+              v-flex(xs6 style="position: relative;")
+                  div(style="position: absolute;z-index: 2;color:#fff;margin:0 5px")  {{ Bskills[i][1] }}%
+                  v-progress-linear(
+                      color="amber lighten-1"
+                      height="20"
+                      :value="Bskills[i][1]"
+                      style="margin:0"
+                  )
+              v-flex(xs12 text-xs-right style="padding-top:0;color:#ce792b"): small {{ Bskills[i][0] }}
+          v-flex(xs2)
+            i( class="fas" :class="[Nskills[i][1]]")
+            div {{ Nskills[i][0] }}  
+          v-flex(xs5)
+            v-layout(row wrap justify-center align-center text-xs-center)
+              v-flex(xs6)
+                div(style="position: absolute;z-index: 2;color:#fff;margin:0 5px") {{skill[1]}}%
+                v-progress-linear(
+                    color="amber lighten-1"
+                    height="20"
+                    :value="skill[1]"
+                    style="margin:0"
+                  )
+              v-flex(xs6)
+                div(style="position: absolute;z-index: 2;color:#fff;margin:0 5px")  {{skill[2]}}%
+                v-progress-linear(
+                    color="red lighten-1"
+                    height="20"
+                    :value="skill[2]"
+                    style="margin:0"
+                  )
+              v-flex(xs12　text-xs-left style="padding-top:0;color:#ce792b"): small {{ Fskills[i][0] }}    
+    
+</template>
+
+<script>
+export default {
+  computed: {
+      Nskills: () => [
+        ["WordPress","fa-check-circle"],
+        ["Vue(nuxt)","fa-check-circle"],
+        ["Laravel","fa-check-circle"],
+        ["Swift","fa-check-circle"],
+      ],
+      Fskills: () => [
+        ["NoThema Full Customize",90,99],
+        ["Vutyfiy/axsios/store/middleware",60,99],
+        ["make website",40,50],
+        ["Todo録音アプリ(※開発のみ)",30,10],
+      ],
+      Bskills:() => [
+        ["MySQL/API/ACF",80,99],
+        ["FirebaseRTDB/Store/Storage/Hosting",70,99],
+        ["MySQL",20,20],
+        ["UserDefault(*Realmは未経験)",10,10],
+      ],
+  },
+}
+</script>
+
+<style>
+
+</style>
