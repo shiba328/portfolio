62ca2f15741bc3e358d92417ba90d2ec0a964c9c
shiba <project.funnal@gmail.com>
2018-10-17 16:43:43 +0900

    indexコンテンツの修正

---
+++ components/index/price.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div(style="text-align: center;" class="mb-5")
+        h1 Price
+        div 受託： 初回/25万円から(継続契約で5万円から)
+        div 就職： 月/35万以上〜(応相談)
+        div インターン：2週間から1ヶ月
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
